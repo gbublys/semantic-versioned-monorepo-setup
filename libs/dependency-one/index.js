c311d4c673c6484e161a064795e1b9c169c37ee6
Gediminas Bublys <gediminas@safty.io>
2024-04-27 22:45:52 +0300

    feat: feat 2

--- libs/dependency-one/index.js
+++ libs/dependency-one/index.js
@@ -1,6 +1,6 @@
 import { value as dependencyFromLibraryTwo } from '@semantic-versioned-monorepo-setup/dependency-two';
 
-// Trigger change 2
+// Trigger change 1
 
 export const dependencyFromLibraryOne = 'dependency-one';
 
