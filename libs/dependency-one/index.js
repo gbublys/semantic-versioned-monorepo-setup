f22a2d470cc534b8cbe615d5d9c9b7d5c563101e
gbublys <37329516+gbublys@users.noreply.github.com>
2024-04-29 12:16:59 +0300

    feat: added feature #3 (#43)

--- libs/dependency-one/index.js
+++ libs/dependency-one/index.js
@@ -1,6 +1,6 @@
 import { value as dependencyFromLibraryTwo } from '@semantic-versioned-monorepo-setup/dependency-two';
 
-export const dependencyFromLibraryOne = 'dependency-one-5';
+export const dependencyFromLibraryOne = 'dependency-one-6';
 
 export default {
     dependencyFromLibraryOne,
