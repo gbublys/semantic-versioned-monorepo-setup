b615b3ae15b61bda0ed84b31c89ca96775e2c13d
Gediminas Bublys <gediminas@safty.io>
2024-04-29 18:34:30 +0300

    feat: feature commit message

--- libs/dependency-one/index.js
+++ libs/dependency-one/index.js
@@ -1,6 +1,6 @@
 import { value as dependencyFromLibraryTwo } from '@semantic-versioned-monorepo-setup/dependency-two';
 
-export const dependencyFromLibraryOne = 'dependency-one-1';
+export const dependencyFromLibraryOne = 'dependency-one-2';
 
 export default {
     dependencyFromLibraryOne,
